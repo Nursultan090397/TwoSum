3b9bc58ffc40510025915b5333a940796898144d
osorov97 <osorov.nursultan97@gmail.com>
2022-10-04 14:01:18 +0600

    1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+    }
+
+    public int[] twoSum(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    return new int[] {i,j};
+                }
+                }
+            }
+        return new int [] {};
     }
 }
